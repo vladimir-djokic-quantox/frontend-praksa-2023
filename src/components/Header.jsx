99d887116e9e6c55ac160f3b8aedbe9d5ea7ff62
Panta87 <panta87kg@gmail.com>
2023-11-10 11:46:14 +0100

    Implemented login and logout functionality.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,22 +5,81 @@ import Sidebar from "./Sidebar.jsx";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
+import LoginModal from "./LoginModal.jsx";
 
 library.add(faBars);
 
 const Header = () => {
   const [open, setOpen] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [userInfo, setUserInfo] = useState({});
+  const [isLoginModalOpen, setIsLoginModalOpen] = useState(false);
+
+  const openLoginModal = () => {
+    setIsLoginModalOpen(true);
+  };
+
+  const closeLoginModal = () => {
+    setIsLoginModalOpen(false);
+  };
+
+  const handleLogin = (userData) => {
+    setUserInfo(userData);
+    setIsLoggedIn(true);
+    closeLoginModal();
+  };
+
+  const handleLogout = () => {
+    setUserInfo({});
+    setIsLoggedIn(false);
+  };
 
   return (
     <header className="bg-gradient-to-r from-green-500 to-green-700 text-white py-4  ">
-      <div className="container mx-auto flex items-center justify-between border-b-2 border-green-400 pb-5">
+      <div className="container mx-auto flex items-center justify-between border-b-2 border-green-400 pb-3">
         <div className="flex items-center">
-          <div className="flex items-center">
-            <img className="w-[60px]  mr-5" src="logo.png" alt="" />
-            <h1 className="text-3xl font-bold">My E-Commerce</h1>
-          </div>
+          <img className="w-[60px] mr-5" src="logo.png" alt="" />
+          <h1 className="text-3xl font-bold">My E-Commerce</h1>
         </div>
+
         <div className="flex items-center space-x-4">
+          <a href="/cart" className="relative group">
+            <FaShoppingCart />
+          </a>
+          <a href="/account">View Account</a>
+          {isLoggedIn ? (
+            <>
+              <p className="text-white">Welcome, {userInfo.firstName}</p>
+              <button onClick={handleLogout}>Logout</button>
+            </>
+          ) : (
+            <button onClick={openLoginModal}>Log In</button>
+          )}
+        </div>
+      </div>
+
+      <div className="container mx-auto mt-3 flex items-center justify-between">
+        <div className="flex items-center space-x-4">
+          <button
+            onClick={() => setOpen(true)}
+            aria-expanded={open}
+            aria-controls="sidebar"
+            aria-label="Open Menu"
+          >
+            <FontAwesomeIcon
+              icon="fa-solid fa-bars"
+              size="2xl"
+              style={{ color: "#ffffff" }}
+            />
+          </button>
+          <Sidebar open={open} setOpen={setOpen} />
+          <p>All Categories</p>
+          <a href="/products" className="">
+            See All Products
+          </a>
+        </div>
+
+        <div className="flex items-center">
           <div className="flex items-center w-[500px] bg-white text-gray-500 rounded-full py-2 px-4">
             <input
               type="text"
@@ -31,37 +90,14 @@ const Header = () => {
               <FiSearch />
             </button>
           </div>
-          <a href="/cart" className="relative group">
-            <FaShoppingCart />
-          </a>
-          <a href="/account" className="">
-            View Account
-          </a>
-          <a href="/login" className="">
-            Log In
-          </a>
         </div>
       </div>
 
-      <div className="container mx-auto mt-3 flex items-center space-x-4">
-        <button
-          onClick={() => setOpen(true)}
-          aria-expanded={open}
-          aria-controls="sidebar"
-          aria-label="Open Menu"
-        >
-          <FontAwesomeIcon
-            icon="fa-solid fa-bars"
-            size="2xl"
-            style={{ color: "#ffffff" }}
-          />
-        </button>
-        <Sidebar open={open} setOpen={setOpen} />
-        <p>All Categories</p>
-        <a href="/products" className="">
-          See All Products
-        </a>
-      </div>
+      <LoginModal
+        isOpen={isLoginModalOpen}
+        onClose={closeLoginModal}
+        onLogin={handleLogin}
+      />
     </header>
   );
 };
---
+++ src/components/LoginModal.jsx
@@ -0,0 +1,101 @@
+import React, { useState, useRef, useEffect } from "react";
+
+const LoginModal = ({ isOpen, onClose, onLogin }) => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const modalRef = useRef(null);
+
+  const handleLogin = () => {
+    fetch("https://dummyjson.com/auth/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        username: username,
+        password: password,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.token) {
+          localStorage.setItem("token", data.token);
+          onLogin(data);
+        } else {
+          alert("Login failed. Check your username and password.");
+        }
+      })
+      .catch((error) => {
+        console.error("Error communicating with the server:", error);
+      });
+  };
+
+  const handleCloseModal = (e) => {
+    if (modalRef.current && !modalRef.current.contains(e.target)) {
+      onClose();
+    }
+  };
+
+  useEffect(() => {
+    if (isOpen) {
+      document.addEventListener("mousedown", handleCloseModal);
+    } else {
+      document.removeEventListener("mousedown", handleCloseModal);
+    }
+
+    return () => {
+      document.removeEventListener("mousedown", handleCloseModal);
+    };
+  }, [isOpen]);
+
+  return (
+    <div
+      ref={modalRef}
+      className={`z-10 fixed top-3 right-3 bg-white p-6 rounded-lg w-[350px] shadow-lg transition-all duration-300 ${
+        isOpen ? "" : "opacity-0 pointer-events-none"
+      }`}
+    >
+      <h2 className="text-2xl font-bold mb-4 text-gray-700">Log In</h2>
+      <form>
+        <div className="mb-4">
+          <label htmlFor="username" className="block text-gray-700">
+            Username:
+          </label>
+          <input
+            type="text"
+            id="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            className="w-full p-2 border border-gray-300 rounded text-gray-700"
+          />
+        </div>
+        <div className="mb-4">
+          <label htmlFor="password" className="block font-medium text-gray-700">
+            Password:
+          </label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className="w-full p-2 border border-gray-300 rounded text-gray-700"
+          />
+        </div>
+        <button
+          type="button"
+          onClick={handleLogin}
+          className="w-full bg-green-500 text-white p-2 rounded hover:bg-green-600 cursor-pointer"
+        >
+          Log In
+        </button>
+      </form>
+      <button
+        onClick={onClose}
+        className="mt-4 text-gray-500 hover:text-gray-700 cursor-pointer"
+      >
+        Close
+      </button>
+    </div>
+  );
+};
+
+export default LoginModal;
